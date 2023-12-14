1a54129ced4dcb15d8624c16eadd9f4e8faa9fad
Jeymen <jeymen11@proton.me>
2023-12-14 21:07:36 +0200

    feat: Improve API.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 import * as dotenv from 'dotenv';
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
+
 dotenv.config();
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
 if (!SUPABASE_URL || !SUPABASE_KEY) {
-    console.error('Supabase URL or Key not provided.');
+    console.error('Supabase API credentials not provided.');
     process.exit(1);
 }
 
@@ -28,21 +29,29 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
-    console.error(err);
+app.use((err, _req, res, _next) => {
     res.status(500).json({ error: 'Internal server error.' });
 });
 
 app.post('/api/checkUsernameAvailability', async (req, res) => {
     try {
         const { username } = req.query;
+
+        if (!username || (username.length as number) <= 0) {
+            return res.status(400).json({
+                error: 'Invalid or missing parameters.',
+            });
+        }
+
         const { data, error } = await supabaseClient
             .from('users')
             .select('username')
             .eq('username', username);
 
         if (error) {
-            throw new Error('Supabase error during username check.');
+            return res
+                .status(500)
+                .json({ error: 'A Supabase error occurred.' });
         }
 
         if (data.length > 0) {
@@ -51,33 +60,54 @@ app.post('/api/checkUsernameAvailability', async (req, res) => {
 
         return res.status(200).json({ message: 'Username is available.' });
     } catch (error) {
-        throw new Error('Error checking username availability.');
+        return res.status(500).json({ error: 'An unknown error occurred.' });
     }
 });
 
 app.post('/api/calculateTotalCodeTime', async (req, res) => {
     try {
         const { userID } = req.query;
+
+        const validUUID =
+            /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[4][0-9a-fA-F]{3}-[8-9a-bA-B][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+        if (!userID || !validUUID.test(userID as string)) {
+            return res.status(400).json({
+                error: 'Invalid or missing parameters.',
+            });
+        }
+
         const { data, error } = await supabaseClient
             .from('sessions')
             .select('time')
             .eq('user_id', userID);
 
-        if (error) {
-            throw new Error(
-                'Supabase error during total code time calculation.',
-            );
+        const { data: userData, error: userError } = await supabaseClient
+            .from('users')
+            .select('user_id')
+            .eq('user_id', userID);
+
+        if (error && userError) {
+            return res.status(500).json({
+                error: 'A Supabase error occurred.',
+            });
         }
 
-        const totalTime =
-            data?.reduce(
-                (acc: number, row: { time: number }) => acc + row.time,
-                0,
-            ) || 0;
+        if (userData.length < 1) {
+            return res.status(404).json({
+                error: 'User not found.',
+            });
+        }
 
-        res.status(200).json({ totalTime });
+        const totalTime = data?.reduce((acc, row) => {
+            if (row && typeof row.time === 'number') {
+                return acc + row.time;
+            }
+            return acc;
+        }, 0);
+
+        return res.status(200).json({ totalTime });
     } catch (error) {
-        throw new Error('Error calculating total code time.');
+        return res.status(500).json({ error: 'An unexpected error occurred' });
     }
 });
 
@@ -86,34 +116,52 @@ app.post('/api/sendDataToSupabase', async (req, res) => {
         const { table, columns, data } = req.body;
 
         if (!table || !columns || !data) {
-            throw new Error('Missing required parameters.');
+            return res.status(400).json({
+                error: 'Missing required parameters.',
+            });
+        }
+
+        if (table.length === 0 || columns.length === 0 || data.length === 0) {
+            return res.status(400).json({
+                error: 'Missing required parameters.',
+            });
         }
 
         const columnsArray = columns.split(',');
 
         if (columnsArray.length !== data.length) {
-            throw new Error('Number of columns does not match data.');
+            return res.status(400).json({
+                error: 'Columns and data must have the same length.',
+            });
         }
 
-        const dataToSend: any = {};
+        if (new Set(columnsArray).size !== columnsArray.length) {
+            return res.status(400).json({
+                error: 'Columns must be unique.',
+            });
+        }
+
+        const dataToSend = {};
         columnsArray.forEach((column, index) => {
             dataToSend[column] = data[index];
         });
 
-        const { data: response, error } = await supabaseClient
-            .from(table as string)
-            .upsert([dataToSend]);
+        const { error } = await supabaseClient.from(table).upsert([dataToSend]);
 
         if (error) {
-            throw new Error('Supabase error during data upsert.');
+            return res
+                .status(500)
+                .json({ error: 'A Supabase error occurred.' });
         }
 
-        res.status(200).json({ success: true });
+        return res
+            .status(200)
+            .json({ message: 'Data sent to Supabase successfully.' });
     } catch (error) {
-        throw new Error('Error processing data for Supabase.');
+        return res.status(500).json({ error: 'An unknown error occurred.' });
     }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
